f0b1f2d39181b589cade1a4b9ee455b79f365637
tramalho <thiagofacul@gmail.com>
2021-10-12 09:09:16 -0300

    init

---
+++ h4x0rnews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  h4x0rnews
+//
+//  Created by Thiago Antonio Ramalho on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ h4x0rnews/h4x0rnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  h4x0rnewsApp.swift
+//  h4x0rnews
+//
+//  Created by Thiago Antonio Ramalho on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct h4x0rnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
